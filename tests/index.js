85e3091f2401888226223594cf547a2e7e84a097
Leon Helwerda <l.s.helwerda@liacs.leidenuniv.nl>
2024-06-21 12:53:27 +0200

    Update Copyright year

--- tests/index.js
+++ tests/index.js
@@ -3,7 +3,7 @@ Unit tests for the visualization fragments.
 
 Copyright 2017-2020 ICTU
 Copyright 2017-2022 Leiden University
-Copyright 2017-2023 Leon Helwerda
+Copyright 2017-2024 Leon Helwerda
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
